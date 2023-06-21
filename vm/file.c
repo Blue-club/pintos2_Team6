c0d344f3a5c94cf13e65666763930b6b5c468261
carrotcookie <a44121078@gmail.com>
2023-06-21 13:54:36 +0000

    임시 do_mmap

--- vm/file.c
+++ vm/file.c
@@ -1,6 +1,10 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
 #include "vm/vm.h"
+/* Project 3 */
+#include <string.h>
+#include "threads/vaddr.h"
+#include "threads/mmu.h"
 
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
@@ -47,12 +51,43 @@ file_backed_destroy (struct page *page) {
 }
 
 /* Do the mmap */
-void *
-do_mmap (void *addr, size_t length, int writable,
-		struct file *file, off_t offset) {
+void *do_mmap (void *addr, size_t length, int writable, struct file *file, off_t offset) {
+	if ((uint64_t)addr % PGSIZE)
+		return NULL;
+
+	file_seek(file, offset);
+	
+	int pg_cnt = 0; // 실제 만들어지는 페이지수는 pg_cnt + 1임
+	off_t read_bytes = 0;
+	void *va = NULL;
+	struct page *page;
+
+	while (length > pg_cnt * PGSIZE) {
+		va = (uint64_t)addr + pg_cnt * PGSIZE;
+		
+		if (spt_find_page(&thread_current()->spt, va) == NULL) {
+			if (vm_alloc_page(VM_FILE, va, writable) == NULL)
+				return NULL;
+		}
+		else
+			return NULL;
+
+		page = spt_find_page(&thread_current()->spt, va);
+		vm_claim_page(page->va);
+		pg_cnt++;
+
+		read_bytes = file_read(file, page->frame->kva, PGSIZE);
+
+		if (read_bytes < PGSIZE)
+			break;
+	}
+
+	memset((uint64_t)page->frame->kva + read_bytes, 0, PGSIZE - read_bytes);
+	file_seek(file, offset);
+	return page->frame->kva;
 }
 
 /* Do the munmap */
-void
-do_munmap (void *addr) {
+void do_munmap (void *addr) {
+
 }
